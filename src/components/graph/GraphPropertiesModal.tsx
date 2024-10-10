12693f2f3d14ee037d243fb4c5abae72a7b98bf7
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-10-10 23:24:39 +0530

    working on logic part

---
+++ src/components/graph/GraphPropertiesModal.tsx
@@ -0,0 +1,98 @@
+import { CloseOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import React from "react";
+
+interface GraphPropertiesModalProps {
+  modalOpen: boolean;
+  handleModalClose: () => void;
+  selectedNode?: {
+    name?: string;
+    properties?: { [key: string]: any };
+  };
+  handleShowMore: (param: any) => void;
+}
+
+const orderedKeys = [
+  "event_type",
+  "event_summary",
+  "expected_minimum_strength",
+  "expected_maximum_strength",
+  "start_event_date",
+  "start_event_time",
+  "end_event_date",
+  "end_event_time",
+  "role_in_event",
+  "designation",
+  "contact_no",
+  "Venue",
+  "Reason",
+  "Nature_of_Event",
+  "Route",
+  "start_point",
+  "mid_points",
+  "end_point",
+];
+
+const formatKey = (key: string) => {
+  return key
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+const GraphPropertiesModal: React.FC<GraphPropertiesModalProps> = ({
+  modalOpen,
+  handleModalClose,
+  selectedNode,
+  handleShowMore,
+}) => {
+  let isAvailable = false;
+
+  return (
+    <Modal
+      title={
+        <span className="text-2xl font-semibold text-center">
+          {selectedNode?.name || "Graph Properties"}
+        </span>
+      }
+      open={modalOpen}
+      onCancel={handleModalClose}
+      footer={
+        <div className="flex justify-end gap-4">
+          <Button onClick={handleShowMore}>Show More</Button>
+        </div>
+      }
+      width={"50%"}
+      centered
+      closeIcon={<CloseOutlined />}
+    >
+      <div className="px-4">
+        <span className="text-lg font-bold">Details</span>
+        <div className="flex flex-col gap-2 mt-1">
+          {selectedNode?.properties &&
+          Object.keys(selectedNode?.properties).length
+            ? orderedKeys.map((key) => {
+                if (key in selectedNode.properties) {
+                  isAvailable = true;
+                  return (
+                    <div key={key} className="flex flex-col">
+                      <span className="text-base font-semibold">
+                        {formatKey(key)}
+                      </span>
+                      <span className="text-base font-semibold text-gray-600">
+                        {selectedNode.properties[key] || "N/A"}
+                      </span>
+                    </div>
+                  );
+                }
+                return null;
+              })
+            : "N/A"}
+          {!isAvailable && <span>N/A</span>}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default GraphPropertiesModal;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <section>
-      <h1>Sidebar</h1>
       <main>
         <Outlet />
       </main>
