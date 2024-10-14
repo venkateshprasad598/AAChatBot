c9ab8840adffdc9a194b5ff37c1fa8efe8c44112
Viram Goyani <vaghasiyab347@gmail.com>
2024-10-14 22:10:44 +0530

    added dark theme color

--- src/components/chatContent/MediaDisplay.tsx
+++ src/components/chatContent/MediaDisplay.tsx
@@ -22,10 +22,10 @@ const MediaDisplay: React.FC<MediaDisplayProps> = ({
         "show-box fixed right-0 bg-bg-100 border-0.5 border-border-300 flex-1 overflow-y-auto bg-white border rounded-xl 2xl:shadow-lg w-full transition-all ease-out duration-150"
       )}
     >
-      <div className="px-4 py-1 border bottom-1 border-t-0 border-l-0 border-r-0 flex items-center gap-3 sticky top-0 show-box-header h-[50px]">
-        <h4 className="mb-0 font-medium text-md">Open Graph</h4>
+      <div className="px-4 py-1 border bottom-1 border-t-0 border-l-0 border-r-0 flex items-center gap-3 sticky top-0 show-box-header h-[50px] bg-white">
+        <h4 className="mb-0 font-medium text-md text-nose">Open Graph</h4>
         <button
-          className="border p-2 rounded hover-bg transition-all ease-out duration-150 ml-auto flex items-center justify-center"
+          className="border p-2 rounded close-icon transition-all ease-out duration-150 ml-auto flex items-center justify-center"
           onClick={hideBox}
         >
           <CloseOutlined />
