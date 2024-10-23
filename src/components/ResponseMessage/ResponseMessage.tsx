c9f137e349e70257133f1917f1050a760a64dec1
venkateshprasad598 <venkateshprasad598@gmail.com>
2024-10-24 01:40:39 +0530

    markdown text added

--- src/components/ResponseMessage/ResponseMessage.tsx
+++ src/components/ResponseMessage/ResponseMessage.tsx
@@ -1,12 +1,17 @@
 import React, { ReactNode } from "react";
 import "./ResponseMessage.css";
+import ReactMarkdown from "react-markdown";
 
 interface ResponseMessageProps {
   userQuestion: ReactNode;
 }
 
+const MarkdownRenderer = ({ markdownText }) => {
+  return <ReactMarkdown children={markdownText} />;
+};
+
 export const ResponseMessage: React.FC<ResponseMessageProps> = ({
   userQuestion,
 }) => {
-  return <h5 className="break-words">{userQuestion}</h5>;
+  return <h5 className="break-words"><MarkdownRenderer markdownText={userQuestion}/></h5>;
 };
